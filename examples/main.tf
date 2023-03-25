44e440bd0e9fed2475741b4cc2907c4dd90b5e38
Master-SC <shibashis.20@gmail.com>
2023-03-25 22:56:41 +0530

    Adding a new test with http_helper

--- examples/main.tf
+++ examples/main.tf
@@ -5,3 +5,7 @@ terraform {
 output "my_ip_address" {
     value="192.168.1.13"
 }
+
+output "demourl" {
+  value = "https://reqres.in/api/users/2"
+}
--- test/terraform_example_test.go
+++ test/terraform_example_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/magiconair/properties"
 	"github.com/stretchr/testify/assert"
@@ -23,8 +25,11 @@ func TestTerraformExample(t *testing.T) {
 
 	// Load the properties File & get the value
 	p := properties.MustLoadFile(path, properties.UTF8)
-	v := p.GetString("data", "null")
+	expectedIp := p.GetString("data", "null")      // Value for Test1 Assertion
+	body := p.GetString("response.body", "null")   //Value for Test2 Response Body
+	responseCode := p.GetInt("response.status", 0) //Value for Test2 status code
 
+	t.Parallel()
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../examples",
@@ -34,6 +39,15 @@ func TestTerraformExample(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	output := terraform.Output(t, terraformOptions, "my_ip_address")
-	assert.Equal(t, v, output)
+	//Test#1 Baics Test to Assert a value.
+
+	actualIp := terraform.Output(t, terraformOptions, "my_ip_address")
+	assert.Equal(t, expectedIp, actualIp)
+
+	//Test#2 To hit a url and check the status code along with response body.
+
+	//func HttpGetWithRetry
+	//func HttpGetWithRetry(t testing.TestingT, url string, tlsConfig *tls.Config, expectedStatus int, expectedBody string, retries int, sleepBetweenRetries time.Duration)
+	url := terraform.Output(t, terraformOptions, "demourl")
+	http_helper.HttpGetWithRetry(t, url, nil, responseCode, body, 5, 5*time.Second)
 }
